01d7f86ee2bf20447096795650d8041b46652a81
Daniel Hampikian <danielhampikian@gmail.com>
2018-07-27 10:07:20 -0600

    basic network

---
+++ Assets/CustomPlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPlayerControls : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+    public float rotSpeed;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotSpeed;
+        translation *= Time.deltaTime; //frame independent movement
+        Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+
+        //modify rigid body for movement
+        rb.MovePosition(rb.position + transform.forward * translation);
+        rb.MoveRotation(rb.rotation * turn);
+	}
+}
---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+    Renderer rend;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        if (isLocalPlayer)
+        {
+            //setup local player controls and stuff
+            GetComponent<CustomPlayerControls>().enabled = true;
+
+            //Set the main Color of the Material to green
+            rend.material.SetColor("_Color", Color.green);
+
+        }
+        else
+        {
+            //deactivate controls and stuff
+            GetComponent<CustomPlayerControls>().enabled = false;
+            rend.material.SetColor("_Color", Color.red);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
